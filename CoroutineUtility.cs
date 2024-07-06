a70a8a97365da207ebf0ec34bf62c97787563ba0
Arya Rakha <33478892+arrakh@users.noreply.github.com>
2024-07-06 21:03:40 +0700

    Coroutine utility

---
+++ CoroutineUtility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Arr.UnityUtils
+{
+    public static class CoroutineUtility
+    {
+        public class EmptyMonoBehaviour : MonoBehaviour {}
+        
+        static EmptyMonoBehaviour _instance;
+
+        [RuntimeInitializeOnLoadMethod]
+        public static void Init()
+        {
+            if (_instance != null) return;
+            GameObject go = new ("Coroutine Utility") {hideFlags = HideFlags.HideInInspector};
+            _instance = go.AddComponent<EmptyMonoBehaviour>();
+            go.hideFlags = HideFlags.HideInHierarchy;
+            Object.DontDestroyOnLoad(go);
+        }
+
+        public static Coroutine Start(IEnumerator routine) => _instance.StartCoroutine(routine);
+        public static void Stop(IEnumerator routine) => _instance.StopCoroutine(routine);
+        public static void Stop(Coroutine routine) => _instance.StopCoroutine(routine);
+    }
+}
